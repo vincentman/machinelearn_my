c644adb6dda1f2428bcc6f885ebfb53d09a16bc7
vincentkao <vincentkao@ibms.sinica.edu.tw>
2018-08-30 09:17:32 +0800

    multi-label

--- multi_label/train.py
+++ multi_label/train.py
@@ -12,7 +12,7 @@ from keras.optimizers import Adam
 from keras.preprocessing.image import img_to_array
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
-from pyimagesearch.smallervggnet import SmallerVGGNet
+from multi_label.pyimagesearch.smallervggnet import SmallerVGGNet
 import matplotlib.pyplot as plt
 from imutils import paths
 import numpy as np
